4b2838f43b5d14db5c2909cfbefadb087d69d3a6
Andrik Kleine <andrik.k@web.de>
2022-07-16 08:55:05 +0200

    Mouse over UI check added

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputHandler : MonoBehaviour
 {
@@ -17,10 +18,31 @@ public class InputHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && !IsMouseOverUIElement())
         {
             Vector2 target = cam.ScreenToWorldPoint(Input.mousePosition);
             playerMovement.Move(new Vector2Int(Mathf.RoundToInt(target.x), Mathf.RoundToInt(target.y)));
         }
+    }
+
+    //Checks if the mouse is currently hovering over UI
+    private bool IsMouseOverUIElement()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+
+        for (int i = 0; i < raycastResults.Count; i++)
+        {
+            RaycastResult curRaycastResult = raycastResults[i];
+
+            if (curRaycastResult.gameObject.layer == LayerMask.NameToLayer("UI"))
+                {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
