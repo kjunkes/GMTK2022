058ff16f1427eb184181f299213a3c4688b6bb95
kjunkes <91727760+kjunkes@users.noreply.github.com>
2022-07-17 14:54:51 +0200

    added Animation John Smith

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Vector3 lastPosition;
+    private Vector3 currentPosition;
+    private Vector3 positionDiff;
+    private int walk;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        lastPosition = gameObject.transform.position;
+        Debug.Log(lastPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = gameObject.transform.position;
+        positionDiff = (currentPosition - lastPosition).normalized;
+        Debug.Log(positionDiff);
+        lastPosition = currentPosition;
+
+        if(positionDiff.x == 1f)
+        {
+            walk = 2;
+        }
+        else if(positionDiff.x == -1f)
+        {
+            walk = 4;
+        }
+        else if(positionDiff.y == 1f)
+        {
+            walk = 1;
+        }
+        else if(positionDiff.y == -1f)
+        {
+            walk = 3;
+        }
+        else
+        {
+            walk = 0;
+        }
+
+        animator.SetInteger("Walk", walk);
+        Debug.Log(walk);
+    }
+}
